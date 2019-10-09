eac0ff7a463874dc61935c91a31f3d233b815fec
Sofiane boubrit <Sofiane.boubrit@gmail.com>
2019-10-09 17:39:59 -0400

    New package

---
+++ app/src/main/java/salhi/boubrit/mayda/MainActivity.java
@@ -0,0 +1,14 @@
+package salhi.boubrit.mayda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
