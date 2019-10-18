1fccbad4de6aa579c8e4dcfb669b006b1cf5abc8
Farouk Salhi <fafous88@gmail.com>
2019-10-18 10:06:28 -0400

    personalisation de la map

--- app/src/main/java/salhi/boubrit/mayda/RestaurantLoginActivity.java
+++ app/src/main/java/salhi/boubrit/mayda/RestaurantLoginActivity.java
@@ -17,6 +17,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -113,6 +114,8 @@ public class RestaurantLoginActivity extends AppCompatActivity {
         });
 
     }
+
+
 
     @Override
     public void onStart() {
--- app/src/main/java/salhi/boubrit/mayda/RestaurantMapActivity.java
+++ app/src/main/java/salhi/boubrit/mayda/RestaurantMapActivity.java
@@ -1,10 +1,16 @@
 package salhi.boubrit.mayda;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,32 +18,67 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class RestaurantMapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ImageButton retour_btn;
     private ImageView gps_view;
+
+    private final static int PERMISSION_REQUEST_ACCES_FINE_LOCATION=1;
+    private Boolean mLocationPermissionGranted;
+    Location rLocation,cLocation,mDernierelocationconnu,mCurrentLocation,mCameraPosition;
+    LocationRequest mlocationRequest;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    //GeoDataClient geoDataClient;
+    //PlaceDetectionClient;
+    private Location mLastknownLocation;
     private static final String TAG = "RestaurantMapActivity";
+
+    private static final LatLng mDefaultLocation = new LatLng(45.526351, -73.587832);
+    private final int DEFAULT_ZOOM = 15;
+    private static final String KEY_CAMERA_POSITION = "camera_position";
+    private static final String KEY_LOCATION = "location";
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_map);
+
+        if (savedInstanceState!=null){
+            mCurrentLocation = savedInstanceState.getParcelable(KEY_LOCATION);
+            mCameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
+        }
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
         mapFragment.getMapAsync(this);
+
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
 
         retour_btn = (ImageButton)findViewById(R.id.ret_btn);
         gps_view = (ImageView)findViewById(R.id.gps_btn);
+
        // getActionBar().setDisplayHomeAsUpEnabled(true);
        // getActionBar().setDisplayShowHomeEnabled(true);
+
         retour_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -48,6 +89,10 @@ public class RestaurantMapActivity extends FragmentActivity implements OnMapRead
         gps_view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                getLocationPermission();
+                UpdateUI();
+                getDeviceLocation();
+               // goToDefaultLocation();
                 Log.d(TAG, "onClick: ");
             }
         });
@@ -66,15 +111,38 @@ public class RestaurantMapActivity extends FragmentActivity implements OnMapRead
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         mMap = googleMap;
 
+        boolean success = googleMap.setMapStyle(new MapStyleOptions(getResources()
+                .getString(R.string.myCustomMap)));
+
+        if (!success) {
+            Log.e(TAG, "Style parsing failed.");
+        }else {
+        getLocationPermission();
+        UpdateUI();
+        getDeviceLocation();}
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
     }
 
-//    @Override
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        if (mMap!=null){
+            outState.putParcelable(KEY_CAMERA_POSITION,mMap.getCameraPosition());
+            outState.putParcelable(KEY_LOCATION,mLastknownLocation);
+            super.onSaveInstanceState(outState, outPersistentState);
+        }
+
+    }
+
+    //    @Override
 //    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 //        int id = item.getItemId();
 //        if (id== android.R.id.home){
@@ -84,10 +152,73 @@ public class RestaurantMapActivity extends FragmentActivity implements OnMapRead
 //    }
 
 
+
+    private void getLocationPermission(){
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+            mLocationPermissionGranted = true;
+        }else ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},PERMISSION_REQUEST_ACCES_FINE_LOCATION);
+
+    }
+
+
+
+    private void UpdateUI(){
+        if (mMap==null){
+            return;
+        }try {
+                      if (mLocationPermissionGranted){
+                            mMap.setMyLocationEnabled(true);
+                            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                        }else {
+                          mMap.setMyLocationEnabled(false);
+                          mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                          mLastknownLocation=null;
+                          getLocationPermission();
+                      }
+        }catch (SecurityException e){
+            Log.e(TAG, "Exception: %s"+ e.getMessage(),e);
+        }
+    }
+
+
+    private void getDeviceLocation(){
+
+        try {
+            if (mLocationPermissionGranted){
+                Task LocationResualt = mFusedLocationProviderClient.getLastLocation();
+                LocationResualt.addOnCompleteListener(this, new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()){
+                            mLastknownLocation = (Location) task.getResult();
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mLastknownLocation.getLatitude(),mLastknownLocation.getLongitude()),DEFAULT_ZOOM));
+                        }else {
+                            Log.d(TAG, "current location is null. Using defaults.");
+                            Log.d(TAG, "Exception %s", task.getException());
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation,DEFAULT_ZOOM));
+                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+        }catch (SecurityException e){
+            Log.e(TAG,"getDeviceLocation: Exception %s "+e.getMessage(),e );
+        }
+    }
+
+
     public void retour(){
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
         finish();
         return;
     }
+
+    private void goToDefaultLocation(){
+        mMap.addMarker(new MarkerOptions().title("Montréal").position(mDefaultLocation));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation,DEFAULT_ZOOM));
+    }
+
+
 }
